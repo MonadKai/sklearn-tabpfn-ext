0a22df886d8110414f7274adb23217155932a33d
Monad Kai <onlookerliu_sjtu@163.com>
2026-07-09 22:53:40 +0800

    api: expose categorical translator helpers

--- sklearn_tabpfn_ext/tabpfn/__init__.py
+++ sklearn_tabpfn_ext/tabpfn/__init__.py
@@ -9,10 +9,18 @@ from sklearn_tabpfn_ext.tabpfn.translate import (
     translate_member,
     translate_sklearn_obj,
 )
+from sklearn_tabpfn_ext.tabpfn.translate import (
+    _categorical_factory as translate_categorical_step,
+)
+from sklearn_tabpfn_ext.tabpfn.translate import (
+    _categorical_indices as categorical_indices,
+)
 
 __all__ = [
     "TranslationProfile",
+    "categorical_indices",
     "profile_for",
+    "translate_categorical_step",
     "translate_input_sanitizer",
     "translate_member",
     "translate_sklearn_obj",
